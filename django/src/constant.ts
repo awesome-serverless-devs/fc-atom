21bb272b7d830d5cb2be8295dd7cd45135e563cd
wss-git <wssgryx@163.com>
2021-03-29 09:35:34 +0800

    feat(django): init django

---
+++ django/src/constant.ts
@@ -0,0 +1,24 @@
+export const HELP = [
+  {
+    header: 'Options',
+    optionList: [
+      {
+        name: 'help',
+        description: 'Use guide',
+        alias: 'h',
+        type: Boolean,
+      },
+    ],
+  },
+  {
+    header: 'Examples',
+    content: [
+      {
+        desc: '使用: deploy',
+        example: '$ s deploy',
+      },
+    ],
+  },
+];
+
+export const CONTEXT = 'PYTHON';
---
+++ django/src/index.ts
@@ -0,0 +1,54 @@
+import { HLogger, ILogger, loadComponent } from '@serverless-devs/core';
+import _ from 'lodash';
+import { CONTEXT } from './constant';
+import transformInputs from './transformInputs';
+
+export default class Component {
+  @HLogger(CONTEXT) logger: ILogger;
+
+  async deploy(inputs) {
+    const webFrameworkInputs = transformInputs(_.cloneDeep(inputs));
+
+    const webFramework = await loadComponent('alibaba/web-framework');
+    return await webFramework.deploy(webFrameworkInputs);
+  }
+
+  async remove(inputs) {
+    const webFrameworkInputs = transformInputs(_.cloneDeep(inputs));
+
+    const webFramework = await loadComponent('alibaba/web-framework');
+    return await webFramework.remove(webFrameworkInputs);
+  }
+
+  async build(inputs) {
+    const webFrameworkInputs = transformInputs(_.cloneDeep(inputs));
+    webFrameworkInputs.Properties.function.runtime = 'python3';
+
+    const webFramework = await loadComponent('alibaba/web-framework');
+    return await webFramework.build(webFrameworkInputs);
+  }
+
+  async ls(inputs) {
+    const webFrameworkInputs = transformInputs(_.cloneDeep(inputs));
+    webFrameworkInputs.Properties.function.runtime = 'python3';
+
+    const webFramework = await loadComponent('alibaba/web-framework');
+    return await webFramework.ls(webFrameworkInputs);
+  }
+
+  async rm(inputs) {
+    const webFrameworkInputs = transformInputs(_.cloneDeep(inputs));
+    webFrameworkInputs.Properties.function.runtime = 'python3';
+
+    const webFramework = await loadComponent('alibaba/web-framework');
+    return await webFramework.rm(webFrameworkInputs);
+  }
+
+  async cp(inputs) {
+    const webFrameworkInputs = transformInputs(_.cloneDeep(inputs));
+    webFrameworkInputs.Properties.function.runtime = 'python3';
+
+    const webFramework = await loadComponent('alibaba/web-framework');
+    return await webFramework.cp(webFrameworkInputs);
+  }
+}
