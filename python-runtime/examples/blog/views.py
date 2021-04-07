ccc8754717fe789e782741db2344cdaabc7aa848
wss-git <wssgryx@163.com>
2021-04-07 11:24:09 +0800

    feat: delete python runtime

--- python-runtime/examples/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse
-from blog.models import Category
-
-# Create your views here.
-def get_category(request):
-  querys = Category.objects.all()
-
-  return HttpResponse(querys)
